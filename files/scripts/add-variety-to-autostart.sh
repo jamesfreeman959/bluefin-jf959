009f946524ee6c8356656b4625f17c73f6361a31
James Freeman <sysadmin@hkskies.com>
2026-01-06 11:50:24 +0000

    Added directory creation if not exists to variety script

--- files/scripts/add-variety-to-autostart.sh
+++ files/scripts/add-variety-to-autostart.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
 set -eou pipefail
- 
+
+if [ ! -d /etc/skel/.config/autostart/ ]; then
+	mkdir -p /etc/skel/.config/autostart/
+fi
+
 cp /usr/share/applications/variety.desktop /etc/skel/.config/autostart/
